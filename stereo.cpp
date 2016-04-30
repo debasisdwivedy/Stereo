a368d809d84dd4f63a9ca6e703a26607d8b4905b
Achyut <achbogga@indiana.edu>
2016-04-30 18:10:02 -0400

    MRF implemented. Results to be verified...!

--- stereo.cpp
+++ stereo.cpp
@@ -27,24 +27,41 @@ class Message_Matirx {
     public:
     vector<CImg<double>> M_from_left;
     vector<CImg<double>> M_from_right;
+    vector<CImg<double>> M_from_u;
+    vector<CImg<double>> M_from_d;
+    
     //Constructor to initialize the message vectors for each pixel
     Message_Matirx (int max_disp, int width, int height) {
         for (int i=0;i<=max_disp;i++){
             CImg<double> l_temp(width, height);
             CImg<double> r_temp(width, height); 
-            Message_Matirx::M_from_left.push_back(l_temp);
-            Message_Matirx::M_from_right.push_back(r_temp);
+            CImg<double> u_temp(width, height);
+            CImg<double> d_temp(width, height);
+            M_from_left.push_back(l_temp);
+            M_from_right.push_back(r_temp);
+            M_from_u.push_back(u_temp);
+            M_from_d.push_back(d_temp);
         }
     }
     double get_message(char from_dir, int d, int i, int j) {
         if (from_dir=='l')
             return M_from_left[d](j,i);
-        else return M_from_right[d](j,i);
+        else if (from_dir=='l')
+            return M_from_left[d](j,i);
+        else if (from_dir=='u')
+            return M_from_u[d](j,i);
+        else if (from_dir=='d')
+            return M_from_d[d](j,i);        
     }
     void set_message(char from_dir, int d, int i, int j, double value) {
         if (from_dir=='l')
             M_from_left[d](j,i) = value;
-        else M_from_right[d](j,i) = value;
+        else if (from_dir=='l')
+            M_from_left[d](j,i) = value;
+        else if (from_dir=='u')
+            M_from_u[d](j,i) = value;
+        else if (from_dir=='d')
+            M_from_d[d](j,i) = value;       
     }
 };
 vector<Message_Matirx> Messages;
@@ -55,7 +72,7 @@ double D_function(const CImg<double> &input1, const CImg<double> &input2, int i,
     for(int ii = max(i-window_size, 0); ii <= min(i+window_size, input1.height()-1); ii++)
         for(int jj = max(j-window_size, 0); jj <= min(j+window_size, input1.width()-1); jj++)
             cost += sqr(input1(min(jj+d, input1.width()-1), ii) - input2(jj, ii));
-    return cost/100;
+    return (cost/100);// coz the forces of D and V need to be balanced perfectly
  //     return cost;
 }
 
@@ -131,26 +148,22 @@ CImg<double> naive_stereo(const CImg<double> &input1, const CImg<double> &input2
 }
 
 
-//compute a message given the direction, the input images, t, i, j, d1 and max_desp
+//compute a message to i,j from the direction given and also given the input images, t, i, j, d1 and max_desp
 double Compute_Message (char dir, const CImg<double> &input1, const CImg<double> &input2, int t, int i, int j, int d1, int max_desp, int ws,double alpha){
     count++;
     if(count==INFINITY)
-        cout<<"iteration :"<<count<<" dir : "<<dir<<"iteration number: "<<t<<endl;;
-    //int ws = 5;
+        cout<<"iteration :"<<count<<" dir : "<<dir<<"iteration number: "<<t<<endl;;//for debugging purposes
     vector<int> d2 = get_d2(d1, max_desp);
     double cost = 0;
     int d;
-  //  double alpha=10;
     pair<int, double> best_disp(0, INFINITY);
-    if ( (t==0) || ((dir=='r')&&((j-1)<0)) || ((dir=='l')&&((j+1)>input1.width())) ) {
-      //  cout<<"base case  ";
+    if ( (t==0) || ((dir=='r')&&((j-1)<0)) || ((dir=='l')&&((j+1)>input1.width())) || ((dir=='d')&&((i-1)<0)) || ((dir=='u')&&((j+1)>input1.height())) ) {
         for(vector<int>::iterator it = d2.begin(); it != d2.end(); ++it) {
             cost= ( D_function(input1, input2, i,j,*it, ws) + V_function(d1,*it,alpha) );//v function constant =30
             d = *it;
             if(cost < best_disp.second)
 				best_disp = make_pair(d, cost);
         }
-//	cout<<best_disp.second<<endl;
         return best_disp.second;
     }
     else if (dir == 'r') {
@@ -163,7 +176,6 @@ double Compute_Message (char dir, const CImg<double> &input1, const CImg<double>
             if(cost < best_disp.second)
             best_disp = make_pair(d, cost);
         }
-  //     	cout<<best_disp.second<<endl;
         return best_disp.second;
     }
     else if (dir == 'l') {
@@ -176,14 +188,37 @@ double Compute_Message (char dir, const CImg<double> &input1, const CImg<double>
             if(cost < best_disp.second)
             best_disp = make_pair(d, cost);
         }
-//	cout<<best_disp.second<<endl; 
+        return best_disp.second;
+    }
+    else if (dir == 'd') {
+		char from_dir = 'u';
+    //    cout<<"sending direction down  ";
+        for(vector<int>::iterator it = d2.begin(); it != d2.end(); ++it) {
+            double cost = 0;
+            cost = ( D_function(input1, input2, i,j,*it, ws) + V_function(d1,*it,alpha) + Messages[t-1].get_message(from_dir, *it, i, j) );
+            d = *it;
+            if(cost < best_disp.second)
+            best_disp = make_pair(d, cost);
+        }
+        return best_disp.second;
+    }
+    else if (dir == 'u') {
+   //     cout<<"sending direction left  ";
+        char from_dir = 'd';
+        for(vector<int>::iterator it = d2.begin(); it != d2.end(); ++it) {
+            double cost = 0;
+            cost = ( D_function(input1, input2, i,j,*it, ws) + V_function(d1,*it,alpha) + Messages[t-1].get_message(from_dir, *it, i, j) );
+            d = *it;
+            if(cost < best_disp.second)
+            best_disp = make_pair(d, cost);
+        }
         return best_disp.second;
     }
 }
 
 //Scanline Stereo BP
 
-//A function to transfer Belief messages and store them
+/*//A function to transfer Belief messages and store them
 void generate_belief (const CImg<double> &input1, const CImg<double> &input2, int window_size, int max_disp, int max_iter,double alpha) {
     //CImg<double> result(input1.width(), input1.height());
     
@@ -210,56 +245,56 @@ void generate_belief (const CImg<double> &input1, const CImg<double> &input2, in
     }
     cout<<"\nBelief Generated";
 }
+*/
 
 void calculate_energy (const CImg<double> &input1, const CImg<double> &input2, int window_size, int max_disp, int max_iter,double alpha) {
     CImg<double> result(input1.width(), input1.height());
 //    double alpha=10;
-    for (int time=0;time<max_iter;time++)
-    {
-	cout<<"\nIteration number: "<<time;
-        Message_Matirx temp(max_disp, input1.width(), input1.height());
-        for(int i=0; i<input1.height(); i++)
-            for(int j=0; j<input1.width(); j++)
-                for (int d=0; d < max_disp; d++)
-	        {
-	            if ((j-1)>0)
-                         temp.set_message('r', d, i, j-1, Compute_Message('l', input1, input2, time, i, j, d, max_disp, window_size,alpha) );
-                    if ((j+1)<input1.width())
-                         temp.set_message('l', d, i, j+1, Compute_Message('r', input1, input2, time, i, j, d, max_disp, window_size,alpha) );
-                }
-	 normalize_message(temp);
-         Messages.push_back(temp);
+    for (int time=0;time<=max_iter;time++){
+		cout<<"\nIteration number: "<<time;
+			Message_Matirx temp(max_disp, input1.width(), input1.height());
+			for(int i=0; i<input1.height(); i++)
+				for(int j=0; j<input1.width(); j++)
+					for (int d=0; d < max_disp; d++)
+					{
+						if ((j-1)>0) temp.set_message('r', d, i, j-1, Compute_Message('l', input1, input2, time, i, j, d, max_disp, window_size,alpha) );
+						if ((j+1)<input1.width()) temp.set_message('l', d, i, j+1, Compute_Message('r', input1, input2, time, i, j, d, max_disp, window_size,alpha) );
+						if ((i-1)>0) temp.set_message('d', d, i-1, j, Compute_Message('u', input1, input2, time, i, j, d, max_disp, window_size,alpha) );
+						if ((i+1)<input1.height()) temp.set_message('u', d, i+1, j, Compute_Message('d', input1, input2, time, i, j, d, max_disp, window_size,alpha) );
+					}
+		 normalize_message(temp);
+		 Messages.push_back(temp);
 
-	 CImg<double> result(input1.width(), input1.height());
-	 for(int i=0; i<input1.height(); i++)
-         {
-		for(int j=0; j<input1.width(); j++)
-                {
-			pair<int, double> best_disp(0, INFINITY);
-			for (int d=0; d < max_disp; d++)
-		        {
-				double cost =( D_function(input1,input2, i, j, d, window_size) + Messages[time].get_message('l', d, i, j) + Messages[time].get_message('r', d, i, j) );
-				if(cost < best_disp.second)
-					best_disp = make_pair(d, cost);
+		 CImg<double> result(input1.width(), input1.height());
+		 for(int i=0; i<input1.height(); i++)
+			 {
+			for(int j=0; j<input1.width(); j++)
+					{
+				pair<int, double> best_disp(0, INFINITY);
+				for (int d=0; d < max_disp; d++)
+					{
+					double cost =( D_function(input1,input2, i, j, d, window_size) + Messages[time].get_message('l', d, i, j) + Messages[time].get_message('r', d, i, j) + Messages[time].get_message('u', d, i, j) + Messages[time].get_message('d', d, i, j) );
+					if(cost < best_disp.second)
+						best_disp = make_pair(d, cost);
+				}
+				result(j,i) = best_disp.first;	
 			}
-			result(j,i) = best_disp.first;	
 		}
-	}
 
-	long double Energy=0;
-	for(int i=0; i<input1.height(); i++)
-        {
-		for(int j=0; j<input1.width(); j++)
-		{
-			Energy+= D_function(input1,input2, i, j, result(j,i), window_size)+(((j-1)>0)?V_function(result(j,i),result(j-1,i), alpha):0)+(((j+1)<input1.width())?V_function(result(j,i),result(j+1,i), alpha):0);
+		long double Energy=0;
+		for(int i=0; i<input1.height(); i++)
+			{
+			for(int j=0; j<input1.width(); j++)
+			{
+				Energy += D_function(input1,input2, i, j, result(j,i), window_size)+ (((j-1)>0)?V_function(result(j,i),result(j-1,i), alpha):0) + (((j+1)<input1.width())?V_function(result(j,i),result(j+1,i), alpha):0) + (((i-1)>0)?V_function(result(j,i),result(j,i-1), alpha):0)+(((i+1)<input1.height())?V_function(result(j,i),result(j,i+1), alpha):0);
+			}
 		}
-	}
-	cout<<"\nEnergy of the iteration "<<time<<" : "<<Energy<<" \n";
+		cout<<"\nEnergy of the iteration "<<time<<" : "<<Energy<<" \n";
     }
     cout<<"\nBelief Generated\n";
 }
 
-
+//BP on ScanLine Stereo HMM
 CImg<double> sl_stereo(const CImg<double> &input1, const CImg<double> &input2, int window_size, int max_disp, int max_iter,double alpha) {
     
     CImg<double> result(input1.width(), input1.height());
@@ -286,18 +321,30 @@ CImg<double> sl_stereo(const CImg<double> &input1, const CImg<double> &input2, i
 }
 
 
-// implement this!
-//  this placeholder just returns a random disparity map
-//
-CImg<double> mrf_stereo(const CImg<double> &img1, const CImg<double> &img2)
+//BP on MRF
+
+CImg<double> mrf_stereo(const CImg<double> &input1, const CImg<double> &input2, int window_size, int max_disp, int max_iter,double alpha)
 {
-  CImg<double> result(img1.width(), img1.height());
-
-  for(int i=0; i<img1.height(); i++)
-    for(int j=0; j<img1.width(); j++)
-      result(j,i) = rand() % 256;
-
-  return result;
+    CImg<double> result(input1.width(), input1.height());
+    
+    calculate_energy(input1, input2, window_size, max_disp, max_iter,alpha);
+    
+    for(int i=0; i<input1.height(); i++)
+    {
+        for(int j=0; j<input1.width(); j++)
+        {
+            pair<int, double> best_disp(0, INFINITY);
+            for (int d=0; d < max_disp; d++)
+	    {
+                double cost =( D_function(input1,input2, i, j, d, window_size) + Messages[max_iter].get_message('l', d, i, j) + Messages[max_iter].get_message('r', d, i, j) + Messages[max_iter].get_message('u', d, i, j) + Messages[max_iter].get_message('d', d, i, j) );
+		if(cost < best_disp.second)
+			best_disp = make_pair(d, cost);
+	    }
+	    result(j,i) = best_disp.first;
+                        //cout<<"\nAfter "<<max_iter<<" iterations for ("<<i<<", "<<j<<")"<<"\nOptimal Cost: "<<cost<<"Optimal Disparity: "<<d;
+        }
+     }
+     return result;
 }
 
 
@@ -306,7 +353,7 @@ int main(int argc, char *argv[])
 {
   if(argc != 4 && argc != 5)
     {
-      cerr << "usage: " << argv[0] << " image_file1 image_file2 [gt_file]" << endl;
+      cerr << "usage: " << argv[0] << " image_file1 image_file2 [gt_file] alpha(potts model constant - good number to start playing with is 200)" << endl;
       return 1;
     }
 
@@ -325,6 +372,7 @@ int main(int argc, char *argv[])
   CImg<double> image1(input_filename1.c_str());
   CImg<double> image2(input_filename2.c_str());
   CImg<double> gt, gt_sl;
+	int MD;
 
   if(gt_filename != "")
   {
@@ -332,35 +380,39 @@ int main(int argc, char *argv[])
     gt_sl = CImg<double>(gt_filename.c_str());
 
     // gt maps are scaled by a factor of 3, undo this...
-    for(int i=0; i<gt.height(); i++)
-      for(int j=0; j<gt.width(); j++)
-        gt(j,i) = gt(j,i) / 3.0;
-        
+    
+    /*for(int i=0; i<gt.height(); i++)
+      for(int j=0; j<gt.width(); j++){
+        gt(j,i) = gt(j,i) / 3.0;    
+        gt_sl(j,i) = gt_sl(j,i) / 3.0;    
+       }
+       * */
     gt_sl.resize(gt.width()/SCALE,gt.height()/SCALE,1,1);//subsampling
-    gt_sl.save((input_filename1 + "-disp_sl.png").c_str());
+    gt_sl.save((input_filename1 + "-disp_gt_downscaled.png").c_str());
+    MD = gt_sl.max();
+    cout<<"\nMD: "<<MD<<endl;
   }
   CImg<double> input1 = image1.get_resize(image1.width()/SCALE, image1.height()/SCALE, 1,1);
     CImg<double> input2 = image2.get_resize(image2.width()/SCALE, image2.height()/SCALE, 1,1);
   // do naive stereo (matching only, no MRF)
-  CImg<double> naive_disp = naive_stereo(input1, input2, 2, 50);
+  CImg<double> naive_disp = naive_stereo(input1, input2, 2, 70);
   naive_disp.get_normalize(0,255).save((input_filename1 + "-disp_naive.png").c_str());
 
-  // do scan line stereo using bp recursively
-  CImg<double> sl_disp = sl_stereo(input1, input2, 2,50,20,a);
-  sl_disp.get_normalize(0,255).save((input_filename1 + "-disp_sl.png").c_str());
+  // do scan line stereo using bp on HMM
+//  CImg<double> sl_disp = sl_stereo(input1, input2, 2, 70, 30, a);//no. of iterations = input1.width()
+//  sl_disp.get_normalize(0,255).save((input_filename1 + "-disp_sl.png").c_str());
 
-/*
-  // do stereo using mrf
-  CImg<double> mrf_disp = mrf_stereo(image1, image2);
+
+  // do stereo using BP on mrf
+  CImg<double> mrf_disp = mrf_stereo(input1, input2, 2, 70, 30, a);
   mrf_disp.get_normalize(0,255).save((input_filename1 + "-disp_mrf.png").c_str());
-*/
+
   // Measure error with respect to ground truth, if we have it...
   if(gt_filename != "")
     {
-      cout << "Naive stereo technique mean error = " << (naive_disp-gt_sl).sqr().sum()/gt.height()/gt.width() << endl;
-      cout << "\nScan Line stereo technique mean error = " << (sl_disp-gt_sl).sqr().sum()/gt_sl.height()/gt_sl.width() << endl;
-      //cout << "MRF stereo technique mean error = " << (mrf_disp-gt).sqr().sum()/gt.height()/gt.width() << endl;
+      cout << "\nNaive stereo technique mean error = " << (naive_disp-gt_sl).sqr().sum()/gt_sl.height()/gt_sl.width() << endl;
+      //cout << "\nScan Line stereo technique mean error = " << (sl_disp-gt_sl).sqr().sum()/gt_sl.height()/gt_sl.width() << endl;
+      cout << "\nMRF stereo technique mean error = " << (mrf_disp-gt_sl).sqr().sum()/gt_sl.height()/gt_sl.width() << endl;
     }
-
   return 0;
 }
